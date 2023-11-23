6d1ffadd1a6ed41750a8f0cc2137a38868614495
antonynapony <tonypoltorabatko@gmail.com>
2023-11-23 22:55:16 +0300

    Add Code to MidtermExam2

---
+++ MidtermExam2/IntExtension.cs
@@ -0,0 +1,59 @@
+﻿//using System.Text;
+
+//namespace MidtermExam2
+//{
+//    public static class IntExtension
+//    {
+//        private static readonly string[] _words = { "ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
+//        public static string GetWords(this int number)
+//        {
+//            var resultBuilder = new StringBuilder();
+//            number = Math.Abs(number);
+//            var numberStr = number.ToString();
+//            foreach (var symbol in numberStr)
+//            {
+//                int index = int.Parse(symbol.ToString());
+//                resultBuilder.Append(_words[index]);
+//                resultBuilder.Append(" ");
+//            }
+
+//            return resultBuilder.ToString().TrimEnd();
+//        }
+//    }
+//}
+
+using System.Text;
+
+namespace MidtermExam2
+{
+    public static class IntExtension
+    {
+        private static readonly Dictionary<char, string> _words = new Dictionary<char, string>()
+        {
+            ['0'] = "ноль",
+            ['1'] = "один",
+            ['2'] = "два",
+            ['3'] = "три",
+            ['4'] = "четыре",
+            ['5'] = "пять",
+            ['6'] = "шесть",
+            ['7'] = "семь",
+            ['8'] = "восемь",
+            ['9'] = "девять",
+            ['-'] = "минус"
+        };
+        public static string GetWords(this int number)
+        {
+            var resultBuilder = new StringBuilder(32);
+            var numberStr = number.ToString();
+            foreach (var symbol in numberStr)
+            {
+                resultBuilder.Append(_words[symbol]);
+                resultBuilder.Append(" ");
+            }
+
+            return resultBuilder.ToString().TrimEnd();
+        }
+
+    }
+}
--- MidtermExam2/Program.cs
+++ MidtermExam2/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MidtermExam2;
+
+Console.WriteLine((565).GetWords());
+
